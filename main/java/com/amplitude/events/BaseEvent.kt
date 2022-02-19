341fcf172673088cd41719bdea653e027b838c87
qingzhuozhen <84748495+qingzhuozhen@users.noreply.github.com>
2022-02-19 13:16:06 -0800

    feat: add revenue helper class (#6)

--- main/java/com/amplitude/events/BaseEvent.kt
+++ main/java/com/amplitude/events/BaseEvent.kt
@@ -3,7 +3,7 @@ package com.amplitude.events
 import org.json.JSONObject
 
 open class BaseEvent : EventOptions() {
-    lateinit var eventType: String
+    open lateinit var eventType: String
     var eventProperties: JSONObject? = null
     var userProperties: JSONObject? = null
     var groups:JSONObject? = null
--- main/java/com/amplitude/events/RevenueEvent.kt
+++ main/java/com/amplitude/events/RevenueEvent.kt
@@ -1,4 +1,7 @@
 package com.amplitude.events
 
+import com.amplitude.Constants
+
 class RevenueEvent : BaseEvent() {
+    override var eventType = Constants.AMP_REVENUE_EVENT
 }
