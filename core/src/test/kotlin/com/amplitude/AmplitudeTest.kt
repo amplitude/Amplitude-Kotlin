61cf64468a8234bca0b3bce3f40896099c369b90
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2022-02-20 23:10:32 -0800

    feat: re-structure and rename main to core module

---
+++ core/src/test/kotlin/com/amplitude/AmplitudeTest.kt
@@ -0,0 +1,12 @@
+package com.amplitude
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class AmplitudeTest {
+
+    @Test
+    fun `test place holder` () {
+        assertTrue(true)
+    }
+}
---
+++ core/src/test/kotlin/com/amplitude/ConfigurationTest.kt
@@ -0,0 +1,12 @@
+package com.amplitude
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ConfigurationTest {
+
+    @Test
+    fun isValid() {
+    }
+}
