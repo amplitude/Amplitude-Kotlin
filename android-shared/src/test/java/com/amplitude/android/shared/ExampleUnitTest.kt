024d5530cf4f23e3c06e43aa99f8d3d8530f97bd
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2022-03-08 13:13:46 -0800

    patch: fix build file

--- android-shared/src/test/java/com/amplitude/android/shared/ExampleUnitTest.kt
+++ android-shared/src/test/java/com/amplitude/android/shared/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.amplitude.android.shared
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
