95e348d00c5c9e888b2c46621e40918047874276
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2022-03-24 15:58:19 -0700

    patch: updates

--- android/src/main/java/com/amplitude/android/Configuration.kt
+++ android/src/main/java/com/amplitude/android/Configuration.kt
@@ -2,11 +2,11 @@ package com.amplitude.android
 
 import android.content.Context
 import com.amplitude.android.utilities.AndroidLoggerProvider
+import com.amplitude.android.utilities.AndroidStorageProvider
 import com.amplitude.core.Configuration
 import com.amplitude.core.EventCallBack
 import com.amplitude.core.LoggerProvider
 import com.amplitude.core.StorageProvider
-import com.amplitude.core.utilities.FileStorageProvider
 
 class Configuration(
     apiKey: String,
@@ -15,7 +15,7 @@ class Configuration(
     flushIntervalMillis: Int = FLUSH_INTERVAL_MILLIS,
     instanceName: String = DEFAULT_INSTANCE,
     optOut: Boolean = false,
-    storageProvider: StorageProvider = FileStorageProvider(),
+    storageProvider: StorageProvider = AndroidStorageProvider(),
     loggerProvider: LoggerProvider = AndroidLoggerProvider(),
     minIdLength: Int? = null,
     callback: EventCallBack? = null,
