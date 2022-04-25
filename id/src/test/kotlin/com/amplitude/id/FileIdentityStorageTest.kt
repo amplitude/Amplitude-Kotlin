92fced72e27a2e30bea2acd179e680096d1895fe
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2022-04-25 11:56:53 -0700

    resolve comments

--- id/src/test/kotlin/com/amplitude/id/FileIdentityStorageTest.kt
+++ id/src/test/kotlin/com/amplitude/id/FileIdentityStorageTest.kt
@@ -9,22 +9,14 @@ class FileIdentityStorageTest {
 
     @Test
     fun `test FileIdentityStorage create and save success`() {
-        val identityStorageProvider = FileIdentityStorageProvider()
-        val identityConfiguration = IdentityConfiguration(
-            instanceName = "testInstance",
-            identityStorageProvider = identityStorageProvider,
-            apiKey = "test-api-key"
-        )
-        val fileIdentityStorage = identityStorageProvider.getIdentityStorage(identityConfiguration)
-        fileIdentityStorage.saveUserId("user_id")
-        fileIdentityStorage.saveDeviceId("device_id")
-        val savedIdentity = fileIdentityStorage.load()
+        val savedIdentity = setUpFileStorage()
         val expectedIdentity = Identity("user_id", "device_id")
         assertEquals(savedIdentity, expectedIdentity)
     }
 
     @Test
     fun `test FileIdentityStorage load from file success`() {
+        setUpFileStorage()
         val identityStorageProvider = FileIdentityStorageProvider()
         val identityConfiguration = IdentityConfiguration(
             instanceName = "testInstance",
@@ -39,6 +31,7 @@ class FileIdentityStorageTest {
 
     @Test
     fun `test FileIdentityStorage load with different apiKey will clear the data`() {
+        setUpFileStorage()
         val identityStorageProvider = FileIdentityStorageProvider()
         val identityConfiguration = IdentityConfiguration(
             instanceName = "testInstance",
@@ -50,5 +43,18 @@ class FileIdentityStorageTest {
         val savedIdentity = fileIdentityStorage.load()
         val expectedIdentity = Identity(null, null)
         assertEquals(savedIdentity, expectedIdentity)
+    }
+
+    private fun setUpFileStorage(): Identity {
+        val identityStorageProvider = FileIdentityStorageProvider()
+        val identityConfiguration = IdentityConfiguration(
+            instanceName = "testInstance",
+            identityStorageProvider = identityStorageProvider,
+            apiKey = "test-api-key"
+        )
+        val fileIdentityStorage = identityStorageProvider.getIdentityStorage(identityConfiguration)
+        fileIdentityStorage.saveUserId("user_id")
+        fileIdentityStorage.saveDeviceId("device_id")
+        return fileIdentityStorage.load()
     }
 }
