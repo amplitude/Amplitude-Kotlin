5810c9d795fe840c254ce050e41d82ed3b9b62af
Alyssa Yu <yuhao900914@gmail.com>
2022-09-29 00:20:18 -0700

    fix: fix other location info not auto populate (#79)

--- android/src/main/java/com/amplitude/android/plugins/AndroidContextPlugin.kt
+++ android/src/main/java/com/amplitude/android/plugins/AndroidContextPlugin.kt
@@ -115,7 +115,13 @@ class AndroidContextPlugin : Plugin {
         if (trackingOptions.shouldTrackCarrier()) {
             event.carrier = contextProvider.carrier
         }
-        if (trackingOptions.shouldTrackCountry()) {
+        if (trackingOptions.shouldTrackIpAddress()) {
+            event.ip ?: let {
+                // get the ip in server side if there is no event level ip
+                event.ip = "\$remote"
+            }
+        }
+        if (trackingOptions.shouldTrackCountry() && event.ip !== "\$remote") {
             event.country = contextProvider.country
         }
         if (trackingOptions.shouldTrackLanguage()) {
@@ -144,10 +150,6 @@ class AndroidContextPlugin : Plugin {
             amplitude.configuration.partnerId ?. let {
                 event.partnerId = it
             }
-        }
-        event.ip ?: let {
-            // get the ip in server side if there is no event level ip
-            event.ip = "\$remote"
         }
         event.plan ?: let {
             amplitude.configuration.plan ?. let {
