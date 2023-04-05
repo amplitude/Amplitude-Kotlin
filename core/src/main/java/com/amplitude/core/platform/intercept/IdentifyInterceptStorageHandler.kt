b869a71b3015d7d342538d1e6c15ef917b1f5b73
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2023-04-05 16:21:47 -0700

    fix lint

--- core/src/main/java/com/amplitude/core/platform/intercept/IdentifyInterceptStorageHandler.kt
+++ core/src/main/java/com/amplitude/core/platform/intercept/IdentifyInterceptStorageHandler.kt
@@ -44,7 +44,7 @@ object IdentifyInterceptorUtil {
         return userProperties
     }
 
-    fun filterNonNullValues(map:  MutableMap<String, Any?>) : MutableMap<String, Any?>{
+    fun filterNonNullValues(map: MutableMap<String, Any?>): MutableMap<String, Any?> {
         return map.filterValues { it != null }.toMutableMap()
     }
 }
