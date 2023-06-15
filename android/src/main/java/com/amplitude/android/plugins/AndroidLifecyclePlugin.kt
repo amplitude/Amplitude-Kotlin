2b916b27389e67c4791e7a140b3bafbc788ac265
Marvin Liu <marvin@amplitude.com>
2023-06-14 17:57:37 -0700

    refactor: use shared casted amplitude and configuration vars

--- android/src/main/java/com/amplitude/android/plugins/AndroidLifecyclePlugin.kt
+++ android/src/main/java/com/amplitude/android/plugins/AndroidLifecyclePlugin.kt
@@ -16,6 +16,8 @@ class AndroidLifecyclePlugin : Application.ActivityLifecycleCallbacks, Plugin {
     override val type: Plugin.Type = Plugin.Type.Utility
     override lateinit var amplitude: Amplitude
     private lateinit var packageInfo: PackageInfo
+    private lateinit var androidAmplitude: com.amplitude.android.Amplitude
+    private lateinit var androidConfiguration: Configuration
 
     private val hasTrackedApplicationLifecycleEvents = AtomicBoolean(false)
     private val numberOfActivities = AtomicInteger(1)
@@ -23,8 +25,10 @@ class AndroidLifecyclePlugin : Application.ActivityLifecycleCallbacks, Plugin {
 
     override fun setup(amplitude: Amplitude) {
         super.setup(amplitude)
+        androidAmplitude = amplitude as com.amplitude.android.Amplitude
+        androidConfiguration = amplitude.configuration as Configuration
 
-        val application = (amplitude.configuration as Configuration).context as Application
+        val application = androidConfiguration.context as Application
         val packageManager: PackageManager = application.packageManager
         packageInfo = try {
             packageManager.getPackageInfo(application.packageName, 0)
@@ -37,44 +41,41 @@ class AndroidLifecyclePlugin : Application.ActivityLifecycleCallbacks, Plugin {
     }
 
     override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
-        val configuration = amplitude.configuration as Configuration
-        if (!hasTrackedApplicationLifecycleEvents.getAndSet(true) && configuration.trackingAppLifecycleEvents) {
+        println(androidConfiguration.trackingDeepLinks)
+        if (!hasTrackedApplicationLifecycleEvents.getAndSet(true) && androidConfiguration.trackingAppLifecycleEvents) {
             numberOfActivities.set(0)
             isFirstLaunch.set(true)
-            DefaultEventUtils(amplitude as com.amplitude.android.Amplitude).trackAppUpdatedInstalledEvent(packageInfo)
+            DefaultEventUtils(androidAmplitude).trackAppUpdatedInstalledEvent(packageInfo)
         }
-        if (configuration.trackingDeepLinks) {
-            DefaultEventUtils(amplitude as com.amplitude.android.Amplitude).trackDeepLinkOpenedEvent(activity)
+        if (androidConfiguration.trackingDeepLinks) {
+            DefaultEventUtils(androidAmplitude).trackDeepLinkOpenedEvent(activity)
         }
     }
 
     override fun onActivityStarted(activity: Activity) {
-        val configuration = amplitude.configuration as Configuration
-        if (configuration.trackingScreenViews) {
-            DefaultEventUtils(amplitude as com.amplitude.android.Amplitude).trackScreenViewedEvent(activity)
+        if (androidConfiguration.trackingScreenViews) {
+            DefaultEventUtils(androidAmplitude).trackScreenViewedEvent(activity)
         }
     }
 
     override fun onActivityResumed(activity: Activity) {
-        (amplitude as com.amplitude.android.Amplitude).onEnterForeground(getCurrentTimeMillis())
+        androidAmplitude.onEnterForeground(getCurrentTimeMillis())
 
-        val configuration = amplitude.configuration as Configuration
         // numberOfActivities makes sure it only fires after activity creation or activity stopped
-        if (configuration.trackingAppLifecycleEvents && numberOfActivities.incrementAndGet() == 1) {
+        if (androidConfiguration.trackingAppLifecycleEvents && numberOfActivities.incrementAndGet() == 1) {
             val isFromBackground = !isFirstLaunch.getAndSet(false)
-            DefaultEventUtils(amplitude as com.amplitude.android.Amplitude).trackAppOpenedEvent(packageInfo, isFromBackground)
+            DefaultEventUtils(androidAmplitude).trackAppOpenedEvent(packageInfo, isFromBackground)
         }
     }
 
     override fun onActivityPaused(activity: Activity) {
-        (amplitude as com.amplitude.android.Amplitude).onExitForeground(getCurrentTimeMillis())
+        androidAmplitude.onExitForeground(getCurrentTimeMillis())
     }
 
     override fun onActivityStopped(activity: Activity) {
-        val configuration = amplitude.configuration as Configuration
         // numberOfActivities makes sure it only fires after setup or activity resumed
-        if (configuration.trackingAppLifecycleEvents && numberOfActivities.decrementAndGet() == 0) {
-            DefaultEventUtils(amplitude as com.amplitude.android.Amplitude).trackAppBackgroundedEvent()
+        if (androidConfiguration.trackingAppLifecycleEvents && numberOfActivities.decrementAndGet() == 0) {
+            DefaultEventUtils(androidAmplitude).trackAppBackgroundedEvent()
         }
     }
 
