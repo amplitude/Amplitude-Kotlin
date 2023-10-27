13a4a19b2e98ac30719c99e37a6f48f8e04b7ad9
Xinyi Ye <xinyi.ye@amplitude.com>
2023-10-27 09:36:48 -0700

    Update HttpClient.kt

--- core/src/main/java/com/amplitude/core/utilities/HttpClient.kt
+++ core/src/main/java/com/amplitude/core/utilities/HttpClient.kt
@@ -14,7 +14,6 @@ import java.net.MalformedURLException
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.Locale
 import java.util.TimeZone
 
 internal class HttpClient(
