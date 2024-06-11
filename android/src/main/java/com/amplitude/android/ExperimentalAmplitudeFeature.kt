2abcfafe317a06ab4eee743914d7f2636e45bc51
pouria.amini <pouria.amini@amplitude.com>
2024-06-11 12:24:31 -0700

    Fix lint issues

--- android/src/main/java/com/amplitude/android/ExperimentalAmplitudeFeature.kt
+++ android/src/main/java/com/amplitude/android/ExperimentalAmplitudeFeature.kt
@@ -2,7 +2,7 @@ package com.amplitude.android
 
 @RequiresOptIn(
     message =
-        "This feature is experimental, and may change or break at any time. Use with caution.",
+    "This feature is experimental, and may change or break at any time. Use with caution.",
 )
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.CONSTRUCTOR)
