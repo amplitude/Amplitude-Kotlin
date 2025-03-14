04aeba8f6c826f8ca730cf2e6379c5bf9025dcd4
paul.intal <paul.intal@amplitude.com>
2025-03-14 13:13:42 -0700

    add a new test with add plugin in between

--- core/src/test/kotlin/com/amplitude/core/platform/MediatorTest.kt
+++ core/src/test/kotlin/com/amplitude/core/platform/MediatorTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
 import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
@@ -26,7 +27,7 @@ class MediatorTest {
 
     @Test
     @Timeout(3, unit = TimeUnit.SECONDS)
-    fun `two threads that call flush twice on two destination plugins`() {
+    fun `call flush twice on two destination plugins`() {
         val fakeDestinationPlugins = List(2) { FakeDestinationPlugin() }
         fakeDestinationPlugins.forEach {
             mediator.add(it)
@@ -50,5 +51,44 @@ class MediatorTest {
         fakeDestinationPlugins.forEach {
             assertEquals(2, it.amountOfWorkDone.get())
         }
+    }
+
+    @Test
+    @Timeout(5, unit = TimeUnit.SECONDS)
+    fun `flush, add a new plugin, and flush again on two destination plugins`() {
+        val fakeDestinationPlugin1 = FakeDestinationPlugin()
+        val fakeDestinationPlugin2 = FakeDestinationPlugin()
+
+        mediator.add(fakeDestinationPlugin1)
+
+        val work = {
+            mediator.applyClosure {
+                (it as EventPlugin).flush()
+            }
+        }
+
+        // flush and add
+        val latch = CountDownLatch(2)
+        val t1 = thread {
+            work()
+            latch.countDown()
+        }
+        val t2 = thread {
+            // add plugin 2, work() should catch up with the newly added plugin
+            mediator.add(fakeDestinationPlugin2)
+            latch.countDown()
+        }
+        latch.await()
+
+        // flush again
+        val t3 = thread {
+            work()
+        }
+        t1.join()
+        t2.join()
+        t3.join()
+
+        assertEquals(2, fakeDestinationPlugin1.amountOfWorkDone.get())
+        assertEquals(2, fakeDestinationPlugin2.amountOfWorkDone.get())
     }
 }
