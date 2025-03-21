f82dd6cdc6411b290b444eeafb529ead0ec3d2a5
paul.intal <paul.intal@amplitude.com>
2025-03-21 12:17:05 -0700

    add retry w/ exponential backoff for cases where an upload request failed - this new logic will respect the order of files as it will always get the next file that we need to upload

---
+++ core/src/test/kotlin/com/amplitude/core/utilities/ExponentialBackoffRetryHandlerTest.kt
@@ -0,0 +1,68 @@
+package com.amplitude.core.utilities
+
+import kotlin.math.pow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ExponentialBackoffRetryHandlerTest {
+
+    @Test
+    fun `canRetry returns properly until maxRetryAttempt is reached`() = runBlocking {
+        val handler = ExponentialBackoffRetryHandler(
+            maxRetryAttempt = 2,
+            baseDelayInMs = 10
+        )
+        repeat(2) {
+            assertTrue(handler.canRetry())
+            handler.retryWithDelay {}
+        }
+        assertFalse(handler.canRetry())
+    }
+
+    @Test
+    fun `retryWithDelay increments attempts within max range`() = runTest {
+        val handler = ExponentialBackoffRetryHandler(maxRetryAttempt = 2)
+
+        handler.retryWithDelay {}
+        assertEquals(1, handler.attempt.get())
+        handler.retryWithDelay {}
+        assertEquals(2, handler.attempt.get())
+        handler.retryWithDelay {}
+        assertEquals(2, handler.attempt.get()) // max attempt reached
+    }
+
+    @Test
+    fun `reset sets attempts to zero`() = runBlocking {
+        val handler = ExponentialBackoffRetryHandler(maxRetryAttempt = 3, baseDelayInMs = 10)
+        repeat(3) {
+            handler.retryWithDelay {}
+        }
+        handler.reset()
+        assertEquals(0, handler.attempt.get())
+    }
+
+    @Test
+    fun `retryWithDelay respects exponential backoff`() = runBlocking {
+        val baseDelayInMs = 10
+        val attemptNumber = 4
+        val handler = ExponentialBackoffRetryHandler(
+            baseDelayInMs = baseDelayInMs
+        )
+        handler.attempt.set(attemptNumber)
+
+        val startTime = System.currentTimeMillis()
+        handler.retryWithDelay {
+            val endTime = System.currentTimeMillis()
+            val elapsedTime = endTime - startTime
+
+            val expected = baseDelayInMs * 2.0.pow(attemptNumber)
+            assertTrue(elapsedTime >= expected)
+        }
+    }
+}
