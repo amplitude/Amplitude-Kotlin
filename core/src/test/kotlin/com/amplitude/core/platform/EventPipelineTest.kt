6d1dbc348bafcdbd98e2263c7211cbb6878fdadb
paul.intal <paul.intal@amplitude.com>
2025-03-21 16:23:56 -0700

    add retry upload logic on EventPipeline - uses ExponentialBackoffRetryHandler to handle most of the logic

--- core/src/test/kotlin/com/amplitude/core/platform/EventPipelineTest.kt
+++ core/src/test/kotlin/com/amplitude/core/platform/EventPipelineTest.kt
@@ -5,15 +5,30 @@ import com.amplitude.core.Configuration
 import com.amplitude.core.State
 import com.amplitude.core.events.BaseEvent
 import com.amplitude.core.utilities.ConsoleLoggerProvider
+import com.amplitude.core.utilities.ExponentialBackoffRetryHandler
 import com.amplitude.core.utilities.InMemoryStorageProvider
+import com.amplitude.core.utilities.http.BadRequestResponse
+import com.amplitude.core.utilities.http.FailedResponse
+import com.amplitude.core.utilities.http.HttpStatus
+import com.amplitude.core.utilities.http.PayloadTooLargeResponse
+import com.amplitude.core.utilities.http.ResponseHandler
+import com.amplitude.core.utilities.http.SuccessResponse
+import com.amplitude.core.utilities.http.TimeoutResponse
+import com.amplitude.core.utilities.http.TooManyRequestsResponse
 import com.amplitude.id.IMIdentityStorageProvider
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 @ExperimentalCoroutinesApi
@@ -37,6 +52,7 @@ class EventPipelineTest {
         storageIODispatcher = testDispatcher,
         retryDispatcher = testDispatcher
     )
+    private val fakeResponseHandler: ResponseHandler = mockk(relaxed= true)
 
     @Test
     fun `should not flush when put and offline`() = runTest(testDispatcher) {
@@ -78,5 +94,70 @@ class EventPipelineTest {
         advanceUntilIdle()
 
         verify(exactly = 1) { eventPipeline.flush() }
+    }
+
+    @Test
+    fun `should reset retry handler on successful upload`() = runTest(testDispatcher) {
+        amplitude.isBuilt.await()
+        val retryUploadHandler = spyk(ExponentialBackoffRetryHandler())
+        val eventPipeline = EventPipeline(
+            amplitude,
+            retryUploadHandler = retryUploadHandler,
+            overrideResponseHandler = fakeResponseHandler
+        )
+        every { fakeResponseHandler.handle(any(), any(), any()) } returns true
+        val event = BaseEvent().apply { eventType = "test_event" }
+
+        eventPipeline.start()
+        eventPipeline.put(event)
+        advanceUntilIdle()
+
+        verify { retryUploadHandler.reset() }
+    }
+
+    @Test
+    fun `should retry on failure`() = runTest(testDispatcher) {
+        amplitude.isBuilt.await()
+        val retryUploadHandler = spyk(ExponentialBackoffRetryHandler())
+        val eventPipeline = EventPipeline(
+            amplitude,
+            retryUploadHandler = retryUploadHandler,
+            overrideResponseHandler = fakeResponseHandler
+        )
+        val event = BaseEvent().apply { eventType = "test_event" }
+
+        eventPipeline.start()
+        eventPipeline.put(event)
+        advanceUntilIdle()
+
+        coVerify { retryUploadHandler.retryWithDelay(any<suspend () -> Unit>()) }
+    }
+
+    @Test
+    fun `should reset after max retry attempts`() = runTest(testDispatcher) {
+        amplitude.isBuilt.await()
+        val retryUploadHandler = spyk(
+            ExponentialBackoffRetryHandler(
+                maxRetryAttempt = 0
+            )
+        )
+        val uploadChannel = Channel<String>(UNLIMITED)
+        uploadChannel.trySend("test1")
+        uploadChannel.trySend("test2")
+        val eventPipeline = EventPipeline(
+            amplitude,
+            retryUploadHandler = retryUploadHandler,
+            overrideResponseHandler = fakeResponseHandler,
+            uploadChannel = uploadChannel
+        )
+        val event = BaseEvent().apply { eventType = "test_event" }
+
+        eventPipeline.start()
+        eventPipeline.put(event)
+        advanceUntilIdle()
+
+        verify { retryUploadHandler.reset() }
+        assertTrue(uploadChannel.isClosedForSend)
+        assertTrue(uploadChannel.isClosedForReceive)
     }
 }
