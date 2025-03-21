19da30f3ac986212e3cc31bb5ca18a98898a3899
paul.intal <paul.intal@amplitude.com>
2025-03-21 12:17:05 -0700

    add retry w/ exponential backoff for cases where an upload request failed - this new logic will respect the order of files as it will always get the next file that we need to upload

---
+++ core/src/main/java/com/amplitude/core/utilities/ExponentialBackoffRetryHandler.kt
@@ -0,0 +1,35 @@
+package com.amplitude.core.utilities
+
+import kotlin.math.pow
+import kotlinx.coroutines.delay
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * A utility class to handle exponential backoff retry logic.
+ */
+class ExponentialBackoffRetryHandler(
+    val maxRetryAttempt: Int = MAX_RETRY_ATTEMPT,
+    private val baseDelayInMs: Int = 1_000,
+    private val factor: Double = 2.0,
+) {
+    internal var attempt = AtomicInteger(0)
+
+    fun canRetry() = attempt.get() < maxRetryAttempt
+
+    suspend fun <T> retryWithDelay(block: suspend () -> T) {
+        if (!canRetry()) return
+        // delay is exponentially increasing based on factor^attempt
+        // factor = 2.0: 1, 2, 4, 8, 16 seconds
+        delay((baseDelayInMs * factor.pow(attempt.get())).toLong())
+        block()
+        attempt.incrementAndGet()
+    }
+
+    fun reset() {
+        attempt.set(0)
+    }
+
+    companion object {
+        private const val MAX_RETRY_ATTEMPT = 5
+    }
+}
