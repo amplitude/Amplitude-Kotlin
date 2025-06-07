fb363cc8a0a7b7f5f3d0edac996391e946658ebf
if <igors.fedulovs@amplitude.com>
2025-06-06 20:52:27 -0700

    refactor: kotlin version bump and release config update

--- release.config.js
+++ release.config.js
@@ -44,7 +44,7 @@ module.exports = {
       "message": "chore(release): ${nextRelease.version} [skip ci]\\n\\n${nextRelease.notes}"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "./gradlew publishReleasePublicationToSonatypeRepository closeAndReleaseSonatypeStagingRepository",
+      "publishCmd": "./gradlew publishReleasePublicationToSonatypeRepository closeAndReleaseStagingRepositories",
     }],
   ],
 }
