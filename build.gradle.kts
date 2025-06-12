ab5c5e5633fc32c948b96a237ea6851507d86572
if <igors.fedulovs@amplitude.com>
2025-06-12 14:27:53 -0700

    refactor: Versions consistency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 buildscript {
-    val kotlinVersion by extra("1.9.25")
-    val dokkaVersion by extra("1.9.20")
     repositories {
         maven(url = "https://plugins.gradle.org/m2/")
         mavenCentral()
@@ -8,8 +6,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.10.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
         classpath("io.github.gradle-nexus:publish-plugin:2.0.0")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.12.2.0")
