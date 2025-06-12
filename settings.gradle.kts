921f03479347f1976af285a898feecee0aa48976
if <igors.fedulovs@amplitude.com>
2025-06-11 20:48:39 -0700

    refactor: Settings kts

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "kotlin-sdk"
+include("core")
+project(":core").projectDir = file("core")
+include("android")
+project(":android").projectDir = file("android")
+include("samples:kotlin-android-app")
+project(":samples:kotlin-android-app").projectDir = file("samples/kotlin-android-app")
+
