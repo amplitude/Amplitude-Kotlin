fee4bbae582699274c2994271249cf2d87d96a35
if <igors.fedulovs@amplitude.com>
2025-07-14 12:01:57 -0700

    fix: version sync

--- samples/kotlin-android-app/build.gradle.kts
+++ samples/kotlin-android-app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExtension.get()
     }
 }
 
