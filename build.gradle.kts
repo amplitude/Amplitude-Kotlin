2f62aab328979092c32f9d12bde6c9e976a5472c
paul.intal <paul.intal@amplitude.com>
2026-03-10 13:33:32 -0700

    add binary compat validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,11 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.bcv)
+}
+
+apiValidation {
+    ignoredProjects += listOf("kotlin-android-app")
 }
 
 allprojects {
