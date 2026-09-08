28d2212dbe070ac707cbeffbfc944881d7c77136
IanMeyer <ian.meyer@amplitude.com>
2026-09-08 06:29:29 -0700

    feat: observe Media3 playback as PlayerEvents

---
+++ streaming-analytics-android/src/main/kotlin/com/amplitude/android/streaming/internal/player/PlayerState.kt
@@ -0,0 +1,6 @@
+package com.amplitude.android.streaming.internal.player
+
+internal data class PlayerState(
+    val isInPictureInPicture: Boolean = false,
+    val isInBackground: Boolean = false,
+)
